d66b264028610d6b3d93b2eb3783529f01f8db23
= <pmuriuki98@gmail.com>
2017-05-26 20:28:38 +0300

    first two lyrics

---
+++ fav/malaika by nyashinski.py
@@ -0,0 +1,46 @@
+__author__ = 'Nyashinski'
+
+"""
+
+Uko na tabia za kupendeza roho
+Tabasamu ya kupapasa macho
+Sauti yako nikiiskia pia natulia yanipunguzia hasira nazo
+Urembo wako ulininasa roho siku ya kwanza kukuona kwa macho
+Sa moyoni umekwamia siwezi  kutania njoo unipunguzie mawazo
+And I feel you in my air when I breath
+I see you in my dreams when I sleep
+You are the one I have been waiting for all my life
+Deep down my heart I believe nakufeel unanifeel hii ni real
+The one I have been waiting for my life
+
+{Chorus}
+Uko sawa na wote malaika na mabawa yao
+Wote wanakuiga we ndo mama yao
+Ni wewe nachagua sisemi nao , sisemi
+Oh baby
+Uko sawa na wote malaika na mabawa yao
+Wote wanakuiga we ndo mama yao
+Wewe nachagua sisemi oh sisemi oh—sisemi
+Uuuuh oh I love you babe
+Oh yeeah yeah yeah iih yeah
+
+
+
+[Verse Two]
+And even though I’ve never been here before somehow
+I know that this is how it feels to fall in love
+Baby you know I love you
+I place no one above you
+I need you in my life forever and ever you will always have my heart
+We were meant to be together me and you
+Oh me and you…yeah
+
+
+{Chorus}
+Uko sawa na wote malaika na mabawa yao
+Wote wanakuiga we ndo mama yao
+Ni wewe nachagua sisemi nao , sisemi
+Oh baby
+Uko sawa na wote malaika na mabawa yao
+Wote wanakuiga we ndo mama yao
+Wewe nachagua sisemi oh sisemi oh—sisemi """
