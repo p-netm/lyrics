0bb5386aa7c2cf9e22cf191579c8b00876740903
= <pmuriuki98@gmail.com>
2017-06-10 00:51:52 +0300

    objectify song attrs

---
+++ define/details.py
@@ -0,0 +1,12 @@
+class SongDetails(object):
+    def __init__(self):
+        """Define a songs details """
+        self.author = None
+        self.singer = None
+        self.title = None
+        self.year = None
+        self.lyrics = None
+        self.genre = None
+        self.country = None
+
+        # i shall continue upgrading this list
